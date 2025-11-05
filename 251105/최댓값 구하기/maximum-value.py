c30208af0043a408800922ded96e10e08261d1aa
Jinsik Jung <toypresent81@gmail.com>
2025-11-05 19:55:02 +0900

    Title: 최댓값 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/최댓값 구하기/maximum-value.py
+++ 251105/최댓값 구하기/maximum-value.py
@@ -1,12 +1,2 @@
 a, b, c = map(int, input().split())
-if a >= b and b >= c:
-    print(a)
-elif a >= b and b <= c:
-    if a >= c:
-        print(a)
-    else:
-        print(c)
-elif a <= b and b >= c:
-    print(b)
-else:
-    print(c)
+print(sorted([a, b, c])[2])
