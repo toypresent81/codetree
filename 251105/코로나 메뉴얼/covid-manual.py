6922898c76cba7c4ec1d6cdbf1bae3465f8dba10
Jinsik Jung <toypresent81@gmail.com>
2025-11-05 19:46:20 +0900

    Title: 코로나 메뉴얼, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/코로나 메뉴얼/covid-manual.py
+++ 251105/코로나 메뉴얼/covid-manual.py
@@ -1,15 +1,8 @@
-info1 = input().split()
-info2 = input().split()
-info3 = input().split()
-
-info = [info1, info2, info3]
-
 cnt = 0
-for i in info:
-    if i[0] == "Y" and int(i[1]) >= 37:
+for _ in range(3):
+    a, b = input().split()
+    if a == "Y" and int(b) >= 37:
         cnt += 1
     else:
         continue
 print("E" if cnt >= 2 else "N")
-
-
