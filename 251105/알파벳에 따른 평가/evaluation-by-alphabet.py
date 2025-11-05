687d8fb5b3d7e94150db89b288292d18c22e93e9
Jinsik Jung <toypresent81@gmail.com>
2025-11-05 15:52:57 +0900

    Title: 알파벳에 따른 평가, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251105/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+char = input()
+if char == "S":
+    print("Superior")
+elif char == "A":
+    print("Excellent")
+elif char == "B":
+    print("Good")
+elif char == "C":
+    print("Usually")
+elif char == "D":
+    print("Effort")
+else:
+    print("Failure")
