e300d1f219545824536beca8561539cb5add97ad
Jinsik Jung <toypresent81@gmail.com>
2025-11-06 10:22:53 +0900

    Title: 100점 도달하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251106/100점 도달하기/reach-100-point.py
@@ -0,0 +1,15 @@
+N = int(input())
+i = N
+while i <= 100:
+    if i >= 90:
+        print("A", end=" ")
+    elif i >= 80:
+        print("B", end=" ")
+    elif i >= 70:
+        print("C", end=" ")
+    elif i >= 60:
+        print("D", end=" ")
+    else:
+        print("F", end=" ")
+    i += 1
+    
