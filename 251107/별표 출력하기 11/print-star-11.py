42161c5501670ff17558d6db7759ca043a2f59d7
Jinsik Jung <toypresent81@gmail.com>
2025-11-07 00:15:53 +0900

    Title: 별표 출력하기 11, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251107/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,8 @@
+N = int(input())
+for i in range(2*N+1):
+    for j in range(2*N+1):
+        if i % 2 == 0 or j % 2 == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
