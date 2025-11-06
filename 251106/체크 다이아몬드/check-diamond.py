e9ddbaa85de79dff0735971f8cdd1c63df67fac2
Jinsik Jung <toypresent81@gmail.com>
2025-11-06 23:33:01 +0900

    Title: 체크 다이아몬드, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251106/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,13 @@
+N = int(input())
+for i in range(1, N+1):
+    for j in range(N-i):
+        print("", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    print()
+for i in range(1, N):
+    for j in range(i):
+        print("", end=" ")
+    for j in range(N-i):
+        print("*", end=" ")
+    print()
