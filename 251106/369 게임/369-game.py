ed5ced8c1684e75dd5b80d97fd6c859bef52b11d
Jinsik Jung <toypresent81@gmail.com>
2025-11-06 10:56:07 +0900

    Title: 369 게임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251106/369 게임/369-game.py
+++ 251106/369 게임/369-game.py
@@ -4,7 +4,10 @@ for i in range(1, N+1):
         print("0", end=" ")
     else:
         if (i % 10) == 0:
-            print(i, end=" ")
+            if (i // 10) % 3 == 0:
+                print("0", end=" ")
+            else:
+                print(i, end=" ")
         else:
             if (i % 10) % 3 == 0:
                 print("0", end=" ")
