7bc0c2755fd10149c884a0a8e1168baecaf654b1
Jinsik Jung <toypresent81@gmail.com>
2025-11-06 17:26:37 +0900

    Title: 출석 부르기 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251106/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+for i in range(100):
+    num = int(input())
+    if num == 1:
+        print("John")
+    elif num == 2:
+        print("Tom")
+    elif num == 3:
+        print("Paul")
+    elif num == 4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
+        
