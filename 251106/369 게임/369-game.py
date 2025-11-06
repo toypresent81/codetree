9dbd618f9673f848a696f1f2029f712cb65ea674
Jinsik Jung <toypresent81@gmail.com>
2025-11-06 10:52:14 +0900

    Title: 369 게임, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251106/369 게임/369-game.py
@@ -0,0 +1,12 @@
+N = int(input())
+for i in range(1, N+1):
+    if i % 3 == 0:
+        print("0", end=" ")
+    else:
+        if (i % 10) == 0:
+            print(i, end=" ")
+        else:
+            if (i % 10) % 3 == 0:
+                print("0", end=" ")
+            else:
+                print(i, end=" ")
