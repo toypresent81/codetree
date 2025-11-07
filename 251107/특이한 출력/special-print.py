ba25e744dfd6008e2e0cc8c3fd44ae8182204209
Jinsik Jung <toypresent81@gmail.com>
2025-11-07 16:50:57 +0900

    Title: 특이한 출력, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251107/특이한 출력/special-print.py
@@ -0,0 +1,8 @@
+N = int(input())
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if (i + j) % 4 == 0:
+            print(f"({i}, {j})", end="\n")
+        else:
+            print(f"({i}, {j})", end=" ")
+        
