a741b0a04e7cb7ef12365823379d882fa7e366f3
Jinsik Jung <toypresent81@gmail.com>
2025-11-07 19:03:48 +0900

    Title: 학점 계산기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251107/학점 계산기/credit-calculator.py
@@ -0,0 +1,10 @@
+N = int(input())
+grades = list(map(float, input().split()))
+avg = sum(grades) / len(grades)
+print(f"{avg:.1f}")
+if avg >= 4.0:
+    print("Perfect")
+elif avg >= 3.0:
+    print("Good")
+else:
+    print("Poor")
