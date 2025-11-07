e1f51c83f177fa4f3fe6c0bbf2b91e7405a9549d
Jinsik Jung <toypresent81@gmail.com>
2025-11-08 08:26:07 +0900

    Title: 주사위 놀이, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251108/주사위 놀이/play-with-dice.py
+++ 251108/주사위 놀이/play-with-dice.py
@@ -1,8 +1,8 @@
 arr = list(map(int, input().split()))
-dices = [0 for _ in range(6)]
+dices = [0 for _ in range(7)]
 
 for i in arr:
-    dices[i-1] += 1
+    dices[i] += 1
 
 for i in range(1, 7):
-    print(f"{i} - {dices[i-1]}")
+    print(f"{i} - {dices[i]}")
