e2719d25b0b33e468bb13b63236dae2a3f07b4a7
Jinsik Jung <toypresent81@gmail.com>
2025-11-07 15:37:42 +0900

    Title: 구구단 만들기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251107/구구단 만들기/print-multiplication-table.py
+++ 251107/구구단 만들기/print-multiplication-table.py
@@ -1,8 +1,8 @@
 A, B = map(int, input().split())
 for i in range(1, 10):
-    for j in range(1, B//2+1):
-        if j == B//2:
-            print(f"{B-2*(j-1)} * {i} = {(B-2*(j-1)) * i}", end=" ")
+    for j in range(B, A-1, -2):
+        if j == A:
+            print(f"{j} * {i} = {j * i}", end=" ")
         else:
-            print(f"{B-2*(j-1)} * {i} = {(B-2*(j-1)) * i}", end=" / ")
+            print(f"{j} * {i} = {j * i}", end=" / ")
     print()
