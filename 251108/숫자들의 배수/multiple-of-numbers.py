3e26fdb66372ed7250b04050531612c9e3073d1a
Jinsik Jung <toypresent81@gmail.com>
2025-11-08 07:37:18 +0900

    Title: 숫자들의 배수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251108/숫자들의 배수/multiple-of-numbers.py
+++ 251108/숫자들의 배수/multiple-of-numbers.py
@@ -1,12 +1,9 @@
 N = int(input())
-arr = []
+arr = [ N * i for i in range(1, N * 11) ]
 cnt = 0
-for i in range(1, 11):
-    arr.append(N * i)
-
 for i in arr:
-    print(i, end=" ")
+    if cnt == 2:        
+        break    
     if i % 5 == 0:
         cnt += 1
-    if cnt == 2:        
-        break
+    print(i, end=" ")
