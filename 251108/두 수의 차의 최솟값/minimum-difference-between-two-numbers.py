eadbbb9df2905624c588a9463eb81ddbccfaded4
Jinsik Jung <toypresent81@gmail.com>
2025-11-08 22:58:44 +0900

    Title: 두 수의 차의 최솟값, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251108/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+min_val = arr[1] - arr[0]
+for i in range(2, n):
+    if min_val > arr[i] - arr[i-1]:
+        min_val = arr[i] - arr[i-1]
+print(min_val)
