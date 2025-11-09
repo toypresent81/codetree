c87330d577c60aaca3a743c198abe9988453f576
Jinsik Jung <toypresent81@gmail.com>
2025-11-09 20:33:03 +0900

    Title: 동전이 있는 위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251109/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+arr_2d = [[0 for _ in range(n)] for _ in range(n)]
+
+for k in range(m):
+    r, c = map(int, input().split())
+    arr_2d[r-1][c-1] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr_2d[i][j], end=" ")
+    print()
