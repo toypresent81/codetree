83b0b5cdfe5a173bdb5f8286edaf1c585e221f65
Jinsik Jung <toypresent81@gmail.com>
2025-11-10 16:28:33 +0900

    Title: 정수만 추출하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251110/정수만 추출하기/extract-only-integers.py
+++ 251110/정수만 추출하기/extract-only-integers.py
@@ -3,8 +3,14 @@ index_a, index_b = 0, 0
 for idx, char in enumerate(a):
     if char.isdigit() == False:
         index_a = idx
+        break
+    else:
+        index_a = len(a)
 for idx, char in enumerate(b):
     if char.isdigit() == False:
         index_b = idx
+        break
+    else:
+        index_b = len(b)
 
 print(int(a[:index_a])+int(b[:index_b]))
