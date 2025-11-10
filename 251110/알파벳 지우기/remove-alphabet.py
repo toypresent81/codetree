f9819e6875bfa3679aa1505437498d7a58aa28c6
Jinsik Jung <toypresent81@gmail.com>
2025-11-10 16:37:50 +0900

    Title: 알파벳 지우기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251110/알파벳 지우기/remove-alphabet.py
@@ -0,0 +1,13 @@
+string_a = input()
+string_b = input()
+
+num_a, num_b = "", ""
+for i in string_a:
+    if i.isdigit() == True:
+        num_a += i
+
+for i in string_b:
+    if i.isdigit() == True:
+        num_b += i
+
+print(int(num_a)+int(num_b))
