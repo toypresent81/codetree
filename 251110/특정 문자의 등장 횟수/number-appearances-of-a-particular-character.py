4e5b9892d5495fee7668a10f2f0aa17a53b5410c
Jinsik Jung <toypresent81@gmail.com>
2025-11-10 11:27:35 +0900

    Title: 특정 문자의 등장 횟수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251110/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 251110/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,3 +1,10 @@
 string = input()
-print(string.count("ee"), end=" ")
-print(string.count("eb"), end=" ")
+countEE = 0
+countEB = 0
+for i in range(len(string)-1):
+    if string[i] + string[i+1] == "ee":
+        countEE += 1
+    if string[i] + string[i+1] == "eb":
+        countEB += 1
+    
+print(countEE, countEB)
