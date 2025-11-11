b5bc4fa81f83a2b40d391aa8fb09cad24412c0be
Jinsik Jung <toypresent81@gmail.com>
2025-11-12 00:32:13 +0900

    Title: 왔다 갔던 구역 2, Time: 78ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251112/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,27 @@
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+blocks = [0 for _ in range(20+1)]
+
+
+start = 0
+end = 0
+for i in range(n):
+    if dir[i] == "L":
+        start = end - x[i]
+    else:
+        end = start + x[i]
+    
+    for j in range(start, end):
+        blocks[j] += 1
+
+count = 0
+for i in blocks:
+    if i >= 2:
+        count += 1
+print(count+1)
