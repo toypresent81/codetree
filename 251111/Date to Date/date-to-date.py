b68981d53992b979099c5308378abf4cbfd558ef
Jinsik Jung <toypresent81@gmail.com>
2025-11-11 20:58:00 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251111/Date to Date/date-to-date.py
+++ 251111/Date to Date/date-to-date.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = map(int, input().split())
 
 # Please write your code here.
-num_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 from_month_total = d1 + 1
 for i in num_of_days[:m1]:
@@ -11,4 +11,4 @@ to_month_total = d2 + 1
 for i in num_of_days[:m2]:
     to_month_total += i
 
-print(to_month_total- from_month_total)
+print(to_month_total- from_month_total + 1)
