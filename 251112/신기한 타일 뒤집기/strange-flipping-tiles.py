29d2a68cbd468e2d772e7c9bb25e530b095e6ebb
Jinsik Jung <toypresent81@gmail.com>
2025-11-12 10:59:22 +0900

    Title: 신기한 타일 뒤집기, Time: 113ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 251112/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+blocks = [''] * 200002
+current = 100000
+
+for _ in range(n):
+    distance, direction = input().split()
+    distance = int(distance)
+    
+    if direction == "L":
+        for j in range(current - distance + 1, current + 1):
+            blocks[j] += "W"
+        current -= (distance - 1) 
+    else:
+        for j in range(current, current + distance):
+            blocks[j] += "B"
+        current += (distance - 1)
+
+w = b = 0
+for block in blocks:
+    w_count = block.count('W')
+    b_count = block.count('B')
+    
+    if block and block[-1] == 'W':
+        w += 1
+    elif block and block[-1] == 'B':
+        b += 1
+
+print(w, b)
