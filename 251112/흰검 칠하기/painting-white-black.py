74b87cb92d4b161e5a1d66368f0c850fbda68934
Jinsik Jung <toypresent81@gmail.com>
2025-11-12 10:46:37 +0900

    Title: 흰검 칠하기, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251112/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+blocks = [''] * 202
+current = 100
+
+for _ in range(n):
+    distance, direction = input().split()
+    distance = int(distance)
+    
+    if direction == "L":
+        for j in range(current - distance + 1, current + 1):
+            blocks[j] += "W"
+        current -= (distance - 1) 
+    else:
+        for j in range(current, current + distance):
+            blocks[j] += "B"
+        current += (distance - 1)
+
+w = b = g = 0
+for block in blocks:
+    w_count = block.count('W')
+    b_count = block.count('B')
+    
+    if w_count >= 2 and b_count >= 2:
+        g += 1
+    elif block and block[-1] == 'W':
+        w += 1
+    elif block and block[-1] == 'B':
+        b += 1
+
+print(w, b, g)
