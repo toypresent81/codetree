f290b4198ef7efc3c1ee238c93fa7917d11d866e
Jinsik Jung <toypresent81@gmail.com>
2025-11-12 16:13:33 +0900

    Title: 연속되는 수 2, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251112/연속되는 수 2/continuous-number2.py
+++ 251112/연속되는 수 2/continuous-number2.py
@@ -1,14 +1,13 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-result = [1]
-count = 1
+
+result, cnt = 0, 0
 for i in range(n):
-    if i == 0 or arr[i] == arr[i-1]:
-        if i == n-1:
-            result.append(count)    
+    if i >= 1 or arr[i] == arr[i-1]:
         count += 1         
-    elif arr[i] != arr[i-1] :
-        result.append(count)
+    else
         count = 1
+    result = max(result, cnt)
+
 print(max(result))
