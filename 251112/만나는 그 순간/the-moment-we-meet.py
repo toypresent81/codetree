bddadbc9d27abdbe3d3aa21286c0054209615777
Jinsik Jung <toypresent81@gmail.com>
2025-11-12 20:44:24 +0900

    Title: 만나는 그 순간, Time: 84ms, Memory: 20MB, Status: Runtime Error - Codetree

---
+++ 251112/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+
+MAX_RANGE = 100000
+ans_a = [0] * (MAX_RANGE + 1)
+ans_b = [0] * (MAX_RANGE + 1)
+
+time_a, time_b = 1, 1
+for _ in range(n):
+    direction, time = input().split()
+    for i in range(int(time)):
+        ans_a[time_a] = ans_a[time_a - 1] + (1 if direction == "R" else -1)
+        time_a += 1
+        
+for _ in range(m):
+    direction, time = input().split()
+    for i in range(int(time)):
+        ans_b[time_b] = ans_b[time_b - 1] + (1 if direction == "R" else -1)
+        time_b += 1
+
+ans = -1
+for i in range(1, min(time_a, time_b)):
+    if ans_a[i] == ans_b[i]:
+        ans = i
+        break
+print(ans)
