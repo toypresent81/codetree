36e60c4186edc423b91be8efe5190207e1b5b631
Jinsik Jung <toypresent81@gmail.com>
2025-11-13 13:40:03 +0900

    Title: 되돌아오기, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251113/되돌아오기/come-back.py
@@ -0,0 +1,40 @@
+N = int(input())
+moves = [tuple(input().split()) for _ in range(N)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+dir_num = 0
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+x, y = 0, 0
+time = 0
+found = False
+
+for i in range(N):
+    for _ in range(dist[i]):
+        if dir[i] == "N":
+            dir_num = 0
+        elif dir[i] == "E":
+            dir_num = 1
+        elif dir[i] == "S":
+            dir_num = 2
+        else:
+            dir_num = 3
+
+        x += dx[dir_num]
+        y += dy[dir_num]
+        time += 1
+
+        if x == 0 and y == 0:
+            found = True
+            break
+    if found:
+        break
+        
+print(time)
+
+
+
+
+
+
