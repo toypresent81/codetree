6084d4657e366ea0372fd8f5ca509dff716eb8a0
Jinsik Jung <toypresent81@gmail.com>
2025-11-13 13:51:34 +0900

    Title: 되돌아오기 2, Time: 65ms, Memory: 20MB, Status: Passed - Codetree

--- 251113/되돌아오기 2/come-back-2.py
+++ 251113/되돌아오기 2/come-back-2.py
@@ -7,6 +7,7 @@ dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
 x, y = 0, 0
 time = 0
+isStart = False
 for dir in dirs:
     if dir == "F":
         x += dx[dir_num]
@@ -18,6 +19,7 @@ for dir in dirs:
 
     time += 1
     if x == 0 and y == 0:
+        isStart = True
         break
 
-print(time)
+print(time if isStart == True else -1)
